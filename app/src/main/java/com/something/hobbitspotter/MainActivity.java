061cfacf66a861ddaf9b70cb8b1e959f1104178d
callags <s.callaghan513@gmail.com>
2020-07-02 10:51:07 -0400

    Permission prompts for app are set.

--- app/src/main/java/com/something/hobbitspotter/MainActivity.java
+++ app/src/main/java/com/something/hobbitspotter/MainActivity.java
@@ -1,21 +1,117 @@
 package com.something.hobbitspotter;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    //Initialize camera button
+    Button camButton;
+
+    static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(OpenCVLoader.initDebug()) {
-            Toast.makeText(getApplicationContext(), "OpenCV loaded successfully", Toast.LENGTH_LONG).show();
+        //Assign camera button to id
+        camButton = findViewById(R.id.camButton);
+
+
+        camButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Check if permission has been granted
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) +
+                        ContextCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.READ_EXTERNAL_STORAGE) +
+                        ContextCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                                PackageManager.PERMISSION_GRANTED) {
+
+                    //When permission is not granted
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.CAMERA) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                        //Create an alert dialog
+                        AlertDialog.Builder builder = new AlertDialog.Builder(
+                                MainActivity.this
+                        );
+                        builder.setTitle("Grant these permissions!");
+                        builder.setMessage("Camera, Read and Write.");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{
+                                                Manifest.permission.CAMERA,
+                                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                                REQUEST_CODE);
+                            }
+                        });
+                        builder.setNegativeButton("Cancel", null);
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{
+                                        Manifest.permission.CAMERA,
+                                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                REQUEST_CODE);
+                    }
+                }
+                //Otherwise load the camera
+                else {
+                    startActivity(new Intent(MainActivity.this, camera.class));
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            //Permissions are allowed
+            if ((grantResults.length > 0 ) && (grantResults[0]
+                    + grantResults[1] + grantResults[2]
+                    == PackageManager.PERMISSION_GRANTED)){
+                //Tell user to press the camera button again to continue
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Press the camera button again to continue");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            //Permissions are denied
+            else {
+                Toast.makeText(getApplicationContext(), "Please allow permissions for this app!", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
---
+++ app/src/main/java/com/something/hobbitspotter/camera.java
@@ -0,0 +1,107 @@
+package com.something.hobbitspotter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class camera<val> extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final int REQUEST_CODE = 1;
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        checkPermission();
+
+    }
+
+    private void checkPermission() {
+        cameraStart();
+    }
+
+    private void cameraStart() {
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                //timestamp at 17:11
+                switch (status) {
+
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+}
